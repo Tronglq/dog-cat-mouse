f929c54e43efa8e0205e0757cd88c2a3b9eb2f56
MT1203 <conchimcon1203@gmail.com>
2020-07-15 11:38:23 +0700

    mouse eat rice

--- Mouse.js
+++ Mouse.js
@@ -8,4 +8,8 @@ Mouse.prototype.sayHi = function() {
     console.log('Hi! I am an Mouse.' + chalk.blue(this.name));
 };
 
+Mouse.prototype.eat = function() {
+    console.log('eat rice');
+}
+
 module.exports = Mouse;
